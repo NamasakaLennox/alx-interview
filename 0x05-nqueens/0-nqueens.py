8222624c65bf5b56145da85af5ee30b3d403bc22
NamasakaLennox <namasakalennox254@gmail.com>
2024-01-12 02:15:15 +0300

    [new]: solve the nqueens problem

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,68 +1,73 @@
 #!/usr/bin/python3
-"""
-Solves the n-queens problem
-This is a N*N chess board where N queens are placed in such a way
-that they cannot capture each other
-"""
+""" N QUEENS ALGORITHM WITH BACKTRACKING (RECURSION INSIDE LOOP) """
+import sys
 
 
-def check_safe(board, row, col):
-    """
-    Checks if position is safe - meaning no other queen can attack it
+class NQueen:
+    """ Class for solving N Queen Problem """
 
-    Args:
-        board: The current state of the board
-        row: the row that is being checked
-        col: the column that is being checked
-    Return:
-        Returns either True or False
-    """
-    # checks if queen can be captured
-    for i in range(col):
-        if board[i] is row or abs(board[i] - row) is abs(i - col):
-            return False
-    return True
+    def __init__(self, n):
+        """ Global Variables """
+        self.n = n
+        self.x = [0 for i in range(n + 1)]
+        self.res = []
+
+    def place(self, k, i):
+        """ Checks if k Queen can be placed in i column (True)
+        or if the are attacking queens in row or diagonal (False)
+        """
+
+        # j checks from 1 to k - 1 (Up to previous queen)
+        for j in range(1, k):
+            # There is already a queen in column
+            # or a queen in same diagonal
+            if self.x[j] == i or \
+               abs(self.x[j] - i) == abs(j - k):
+                return 0
+        return 1
+
+    def nQueen(self, k):
+        """ Tries to place every queen in the board
+        Args:
+        k: starting queen from which to evaluate (should be 1)
+        """
+        # i goes from column 1 to column n (1st column is 1st index)
+        for i in range(1, self.n + 1):
+            if self.place(k, i):
+                # Queen can be placed in i column
+                self.x[k] = i
+                if k == self.n:
+                    # Placed all 4 Queens (A solution was found)
+                    solution = []
+                    for i in range(1, self.n + 1):
+                        solution.append([i - 1, self.x[i] - 1])
+                    self.res.append(solution)
+                else:
+                    # Need to place more Queens
+                    self.nQueen(k + 1)
+        return self.res
 
 
-def check_board(board, col):
-    """
-    Checks the board state of the column using backtracking
+# Main
 
-    Args:
-        board: the state of the board
-        col: the column being checked
-    """
-    n = len(board)
-    if col is n:  # if all items have been iterated
-        print(str([[c, board[c]] for c in range(n)]))
-        return
+if len(sys.argv) != 2:
+    print("Usage: nqueens N")
+    sys.exit(1)
 
-    for row in range(n):  # iterate checking if position is safe
-        if check_safe(board, row, col):
-            board[col] = row
-            check_board(board, col + 1)
+N = sys.argv[1]
 
+try:
+    N = int(N)
+except ValueError:
+    print("N must be a number")
+    sys.exit(1)
 
-if __name__ == "__main__":
-    """
-    driver of the program
-    """
-    import sys
+if N < 4:
+    print("N must be at least 4")
+    sys.exit(1)
 
-    if len(sys.argv) != 2:
-        print("usage: nqueens N")
-        sys.exit(1)
+queen = NQueen(N)
+res = queen.nQueen(1)
 
-    n = 0
-    try:
-        n = int(sys.argv[1])
-    except Exception:
-        print("N must be a number")
-        sys.exit(1)
-
-    if n < 4:
-        print("N must be at least 4")
-        sys.exit(1)
-
-    board = [0 for col in range(n)]
-    check_board(board, 0)
+for i in res:
+    print(i)
