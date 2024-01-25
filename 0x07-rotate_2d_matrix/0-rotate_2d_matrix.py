8a55bf00a09f2baddbd5bda2e90b5bf8191ff520
NamasakaLennox <namasakalennox254@gmail.com>
2024-01-25 22:52:11 +0300

    new: implemented a 2d matrix rotation in place

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""rotating a 2D matrix
+"""
+
+
+def rotate_2d_matrix(matrix):
+    """A function that rotates a 2d matrix
+    """
+    N = len(matrix)
+
+    for i in range(N//2):
+        for j in range(i, N - i - 1):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[N - 1 - j][i]
+            matrix[N - 1 - j][i] = matrix[N - 1 - i][N - 1 - j]
+            matrix[N - 1 - i][N - 1 - j] = matrix[j][N - 1 - i]
+            matrix[j][N - 1 - i] = temp
